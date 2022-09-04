302fc94533b83c6c88a58eee54ad005385db7d36
Chekko Milani <Chekko.Milani@kennedysiq.com>
2022-09-04 16:13:54 +0100

    Tests cleanup

--- src/EazyHttp/EazyHttp.Tests/Usings.cs
+++ src/EazyHttp/EazyHttp.Tests/Usings.cs
@@ -2,3 +2,6 @@ global using FluentAssertions;
 global using Microsoft.Extensions.DependencyInjection;
 global using System.Web;
 global using Xunit;
+global using Moq;
+global using EazyHttp.Contracts;
+global using Microsoft.Extensions.Options;
