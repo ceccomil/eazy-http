d8560327652a06698e978551d5fa1e01a3b5173b
ceccomil <40296947+ceccomil@users.noreply.github.com>
2022-10-15 20:13:20 +0100

    Feature/handler example (#20)
    * example
    
    * Release v.1.2.1
    
    Co-authored-by: Chekko Milani <milani.francesco@outlook.com>

---
+++ src/Examples/CryptoPrices/CustomHttpHandler.cs
@@ -0,0 +1,45 @@
+﻿using CaptainLogger;
+using System.Net;
+
+namespace CryptoPrices;
+
+public class CustomHttpHandler : HttpClientHandler
+{
+    private readonly ICaptainLogger _logger;
+
+    public CustomHttpHandler(
+        ICaptainLogger<CustomHttpHandler> logger)
+        : base()
+    {
+        _logger = logger;
+
+        AutomaticDecompression = DecompressionMethods.All;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        _logger
+            .InformationLog(
+                request.Method,
+                request.RequestUri);
+
+        var result = await base
+            .SendAsync(
+                request,
+                cancellationToken);
+
+        _logger
+            .InformationLog(
+                (int)result.StatusCode,
+                result.ReasonPhrase,
+                result.Version,
+                result
+                    .Content
+                    .GetType()
+                    .Name);
+
+        return result;
+    }
+}
---
+++ src/Examples/CryptoPrices/Program.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using EazyHttp;
+using EazyHttp.HttpClients;
+using CaptainLogger;
+using CaptainLogger.Options;
+using Microsoft.Extensions.Logging;
+using EazyHttp.Contracts;
+using CryptoPrices;
+
+var services = new ServiceCollection()
+    .ConfigureEazyHttpClients(opts =>
+    {
+        opts
+            .EazyHttpClients
+            .Add(new(
+                "MessariClient",
+                "https://data.messari.io/api/v2"));
+
+        opts
+            .HttpClientHandlers
+            .Add(
+                "MessariClient",
+                "CryptoPrices.CustomHttpHandler");
+    })
+    .AddEazyHttpClients()
+    .Configure<CaptainLoggerOptions>(opts =>
+    {
+        opts.TimeIsUtc = true;
+        opts.LogRecipients = Recipients.Console;
+        opts.ArgumentsCount = LogArguments.Four;
+
+        opts.Templates.Add(
+            LogArguments.Two,
+            "Sending request [{HttpMethod}] {Uri}");
+
+        opts.Templates.Add(
+                LogArguments.Three,
+                "Today's {Position} coin is:\r\n{Slug} at {Price:N10} USD");
+
+        opts.Templates.Add(
+                LogArguments.Four,
+                "Response [{Code}] - {Phrase} Http version " +
+                "{Version}\r\nContent class type: {ContentEncoding}");
+
+    })
+    .AddLogging(builder =>
+    {
+        builder
+            .ClearProviders()
+            .AddCaptainLogger()
+            .AddFilter(
+                "",
+                LogLevel.Information);
+    })
+    .AddTransient<CustomHttpHandler>();
+
+
+using var sp = services
+    .BuildServiceProvider();
+
+using var scope = sp
+    .CreateScope();
+
+var logger = sp
+    .GetRequiredService<ICaptainLogger<MessariClient>>();
+
+logger
+    .InformationLog("Application is starting");
+
+logger
+    .InformationLog("Getting Eazy Http clients from DI container");
+
+var coinClient = sp
+    .GetRequiredService<IEazyClients>()
+    .MessariClient;
+
+var query = new HttpQuery();
+
+logger
+    .InformationLog(
+        "Setting up query parameters");
+
+query.AddParam(new("fields", "id,slug,symbol,metrics/market_data/price_usd"));
+
+var result = await coinClient
+    .GetAsync<ResultDto>(
+        "assets",
+        query)
+    ?? throw new NullReferenceException();
+
+var max = result
+    .Data
+    .MaxBy(x => x.Metrics.MarketData.PriceUsd)
+    ?? throw new NullReferenceException();
+
+logger
+    .InformationLog(
+        "Max valued",
+        max.Slug,
+        max.Metrics.MarketData.PriceUsd);
+
+var min = result
+    .Data
+    .MinBy(x => x.Metrics.MarketData.PriceUsd)
+    ?? throw new NullReferenceException();
+
+logger
+    .InformationLog(
+        "Min valued",
+        min.Slug,
+        min.Metrics.MarketData.PriceUsd);
