f0ef6a9908c271493a524636f18bce13c279f461
Chekko Milani <milani.francesco@outlook.com>
2022-10-15 11:38:10 +0100

    example

---
+++ src/Examples/BitcoinPriceIndex/Program.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using EazyHttp;
+using EazyHttp.HttpClients;
+using CaptainLogger;
+using CaptainLogger.Options;
+using Microsoft.Extensions.Logging;
+using EazyHttp.Contracts;
+using BitcoinPriceIndex;
+
+var services = new ServiceCollection()
+    .ConfigureEazyHttpClients(opts =>
+    {
+        opts
+            .EazyHttpClients
+            .Add(new(
+                "MessariClient",
+                "https://data.messari.io/api/v2"));
+    })
+    .AddEazyHttpClients()
+    .Configure<CaptainLoggerOptions>(opts =>
+    {
+        opts.TimeIsUtc = true;
+        opts.LogRecipients = Recipients.Console;
+        opts.ArgumentsCount = LogArguments.Three;
+        opts.Templates.Add(
+                LogArguments.Three,
+                "Today's {Position} coin is:\r\n{Slug} at {Price:N10} USD");
+
+    })
+    .AddLogging(builder =>
+    {
+        builder
+            .ClearProviders()
+            .AddCaptainLogger()
+            .AddFilter(
+                "",
+                LogLevel.Information);
+    });
+
+
+using var sp = services
+    .BuildServiceProvider();
+
+using var scope = sp
+    .CreateScope();
+
+var logger = sp
+    .GetRequiredService<ICaptainLogger<MessariClient>>();
+
+logger
+    .InformationLog("Application is starting");
+
+logger
+    .InformationLog("Getting Eazy Http clients from DI container");
+
+var coinClient = sp
+    .GetRequiredService<IEazyClients>()
+    .MessariClient;
+
+var query = new HttpQuery();
+
+logger
+    .InformationLog(
+        "Setting up query parameters");
+
+query.AddParam(new("fields", "id,slug,symbol,metrics/market_data/price_usd"));
+
+logger
+    .InformationLog("Sending request");
+
+var result = await coinClient
+    .GetAsync<ResultDto>(
+        "assets",
+        query)
+    ?? throw new NullReferenceException();
+
+var max = result
+    .Data
+    .MaxBy(x => x.Metrics.MarketData.PriceUsd)
+    ?? throw new NullReferenceException();
+
+logger
+    .InformationLog(
+        "Max valued",
+        max.Slug,
+        max.Metrics.MarketData.PriceUsd);
+
+var min = result
+    .Data
+    .MinBy(x => x.Metrics.MarketData.PriceUsd)
+    ?? throw new NullReferenceException();
+
+logger
+    .InformationLog(
+        "Min valued",
+        min.Slug,
+        min.Metrics.MarketData.PriceUsd);
---
+++ src/Examples/BitcoinPriceIndex/ResultDto.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BitcoinPriceIndex;
+
+public class ResultDto
+{
+    public StatusDto Status { get; set; } = null!;
+    public IEnumerable<DataDto> Data { get; set; } = null!;
+
+}
+
+public class StatusDto
+{
+    public double Elapsed { get; set; }
+    public DateTimeOffset Timestamp { get; set; }
+}
+
+public class DataDto
+{
+    public Guid Id { get; set; }
+    public string Slug { get; set; } = null!;
+    public string Symbol { get; set; } = null!;
+    public MetricsDto Metrics { get; set; } = null!;
+}
+
+public class MetricsDto
+{
+    [JsonPropertyName("market_data")]
+    public MarketDataDto MarketData { get; set; } = null!;
+}
+
+public class MarketDataDto
+{
+    [JsonPropertyName("price_usd")]
+    public decimal PriceUsd { get; set; }
+}
